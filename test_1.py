6c378f27280c4089e130af5010693b690d0325ba
Maxim Chereshnyuk <mlchereshnyuk@sberbank.ru>
2022-02-01 11:11:59 +0300

    update test_1.py

--- test_1.py
+++ test_1.py
@@ -1,14 +1,10 @@
-from datetime import datetime, timedelta
+from datetime import datetime
 from airflow import DAG
 from airflow.operators.bash import BashOperator
-from airflow.operators.dummy import DummyOperator
-dag = DAG(
-    dag_id='dag_123',
-    schedule_interval='0 0 * * *',
-    start_date=datetime(2021, 1, 1),
-    catchup=False)
 
-one = BashOperator(task_id='test_1', bash_command='echo "$TEST_VAR"', dag=dag)
-two = BashOperator(task_id='test_2', bash_command='echo "$TEST_VAR_2"', dag=dag)
-three = BashOperator(task_id='test_3', bash_command='echo "$PWD" && ls', dag=dag)
-one >> two >> three
+
+with DAG(dag_id='dag_to_test', schedule_interval='0 0 * * *', start_date=datetime(2022, 1, 25), catchup=False) as dag:
+    one = BashOperator(task_id='task_1', bash_command='echo $TEST_VAR')
+    two = BashOperator(task_id='task_2', bash_command='echo $TEST_VAR_2')
+    three = BashOperator(task_id='task_3', bash_command='echo $PWD && ls -la')
+    one >> two >> three
